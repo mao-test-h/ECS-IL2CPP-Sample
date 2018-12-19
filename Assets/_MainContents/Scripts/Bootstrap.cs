6f6f7936728c9bb41c9ca303479768128fbd4267
mao <miymao78.d@gmail.com>
2018-12-20 01:31:04 +0900

    add project

---
+++ Assets/_MainContents/Scripts/Bootstrap.cs
@@ -0,0 +1,88 @@
+﻿namespace MainContents
+{
+    using UnityEngine;
+
+    using Unity.Entities;
+    using Unity.Mathematics;
+    using Unity.Transforms;
+    using Unity.Rendering;
+
+    using UnityRandom = UnityEngine.Random;
+
+    public sealed class Bootstrap : MonoBehaviour
+    {
+#pragma warning disable 0649
+        // ------------------------------
+        #region // Private Members(Editable)
+
+        [SerializeField] int _maxObjectNum = 10000;
+        [SerializeField] float _boundSize = 128f;
+        [SerializeField] MeshInstanceRenderer _meshInstanceRenderer;
+
+        #endregion // Private Members(Editable)
+#pragma warning restore 0649
+
+        // ----------------------------------------------------
+        #region // Unity Events
+
+        /// <summary>
+        /// MonoBehaviour.Start
+        /// </summary>
+        void Start()
+        {
+            // Create World & Systems.
+            World.Active = new World("Sample World");
+            var entityManager = World.Active.CreateManager<EntityManager>();
+            // Built-in System
+            World.Active.CreateManager(typeof(EndFrameTransformSystem));
+            World.Active.CreateManager(typeof(RenderingSystemBootstrap));
+            // Original System
+            World.Active.CreateManager(typeof(RotationSytstem));
+            ScriptBehaviourUpdateOrder.UpdatePlayerLoop(World.Active);
+
+            // Create Archetype.
+            var archetype = entityManager.CreateArchetype(
+                ComponentType.Create<Prefab>(),
+                ComponentType.Create<Position>(),
+                ComponentType.Create<Rotation>(),
+                ComponentType.Create<LocalToWorld>(),
+                ComponentType.Create<MeshInstanceRenderer>());
+
+            // Create Prefab Entities.
+            var prefabEntity = entityManager.CreateEntity(archetype);
+            entityManager.SetSharedComponentData(prefabEntity, this._meshInstanceRenderer);
+
+            // Create Entities.
+            for (int i = 0; i < _maxObjectNum; ++i)
+            {
+                var entity = entityManager.Instantiate(prefabEntity);
+                entityManager.SetComponentData(entity, new Position { Value = this.GetRandomPosition() });
+                entityManager.SetComponentData(entity, new Rotation { Value = UnityEngine.Random.rotation });
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour.OnDestroy
+        /// </summary>
+        void OnDestroy()
+        {
+            World.DisposeAllWorlds();
+        }
+
+        #endregion // Unity Events
+
+        // ----------------------------------------------------
+        #region // Private Methods
+
+        float3 GetRandomPosition()
+        {
+            float halfSize = this._boundSize / 2f;
+            return new float3(
+                UnityRandom.Range(-halfSize, halfSize),
+                UnityRandom.Range(-halfSize, halfSize),
+                UnityRandom.Range(-halfSize, halfSize));
+        }
+
+        #endregion // Private Methods
+    }
+}
---
+++ Assets/_MainContents/Scripts/ECS/RotationSytstem.cs
@@ -0,0 +1,30 @@
+﻿namespace MainContents
+{
+    using UnityEngine;
+
+    using Unity.Entities;
+    using Unity.Mathematics;
+    using Unity.Transforms;
+    using Unity.Jobs;
+    using Unity.Burst;
+
+    /// <summary>
+    /// 回転するだけのシステム
+    /// </summary>
+    public sealed class RotationSytstem : JobComponentSystem
+    {
+        [BurstCompile]
+        public struct RotationJob : IJobProcessComponentData<Rotation>
+        {
+            float _deltaTime;
+            public RotationJob(float deltaTime) => this._deltaTime = deltaTime;
+            public void Execute(ref Rotation rot)
+            {
+                rot.Value = math.mul(
+                    math.normalize(rot.Value),
+                    quaternion.AxisAngle(math.up(), 2f * this._deltaTime));
+            }
+        }
+        protected override JobHandle OnUpdate(JobHandle inputDeps) => new RotationJob(Time.deltaTime).Schedule(this, inputDeps);
+    }
+}
